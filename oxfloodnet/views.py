960aa9ba7d190006e161b38d8635a00efa713b12
J-P Stacey <jp.stacey@gmail.com>
2013-11-02 12:25:56 +0000

    Stub out JSON view

--- oxfloodnet/views.py
+++ oxfloodnet/views.py
@@ -1,6 +1,21 @@
-from flask import render_template
+#!/usr/bin/env python
+# @file views.py
+#
+# Routing for oxfloodnet
+
+from flask import render_template, json
 from oxfloodnet import oxfloodnet
 
 @oxfloodnet.route('/')
 def index():
-	return render_template('index.html')
+    """
+    Return index HTML for human beings
+    """
+    return render_template('index.html')
+
+@oxfloodnet.route('/data/<ne>/<sw>')
+def return_data(ne, sw):
+    """
+    Return JSON data based on bounding box
+    """
+    return json.jsonify(request = {'ne':ne,'sw':sw})
