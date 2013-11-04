508bed797a49701a55f9da42c89c358d0e5191d7
Max Glenister <moglenstar@gmail.com>
2013-11-04 17:41:54 +0000

    Refactored the front-end javascript to be less spaghetti-like

--- oxfloodnet/static/js/main.js
+++ oxfloodnet/static/js/main.js
@@ -1,90 +1,83 @@
-$('.non-geo-switch a').on('click', function (e){
-    e.preventDefault();
+/*global $:false */
+var core = core || {};
 
-    $('.non-geo-switch').hide();
-    $('.non-geo-form').show();
-    $('.status').hide();
-});
-$('.non-geo-form a').on('click', function (e){
-    e.preventDefault();
+(function(module){
+    'use strict';
+    var handle_location_error = function () {
+        $('.status.error').show();
+    };
 
-    $('.non-geo-switch').show();
-    $('.non-geo-form').hide();
-});
 
-var get_location = function () {
-    if (Modernizr.geolocation) {
-        navigator.geolocation.getCurrentPosition(show_map, handle_status);
-    } else {
-        $('.non-geo-switch').hide();
-        $('.non-geo-form').show();
-    }
-};
-var show_map = function (position) {
-    $('.success').show();
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    var latlng = [latitude, longitude];
+    var send_location_to_application = function (map) {
+        var bounds = map.getBounds();
+        var latlon = map.getCenter();
 
-    var map = L.map('map', {
-        center: latlng,
-        zoom: 13
-    });
-    var googleLayer = new L.Google('TERRAIN');
-    map.addLayer(googleLayer);
+        var url = '/data/'+latlon.lat+','+latlon.lng+'/'+bounds._southWest.lat+','+bounds._southWest.lng+'/'+bounds._northEast.lat+','+bounds._northEast.lng;
 
-    var marker = L.marker(latlng).addTo(map);
+        $.ajax({
+            url: url
+        }).done(function (data){
+            update_map(map, data.data);
+        });
+    };
 
-    send_location(map);
 
-};
-var handle_status = function (status) {
-    if (status.code === 1) {
-        $('.error').show();
-    }
-};
-var generate_heatmap = function(map, data) {
-    var heatmapLayer = L.TileLayer.heatMap({
-        radius: {
-            value: 200,
-            absolute: true  // true: radius in meters, false: radius in pixels
-        },
-        opacity: 0.5,
-        gradient: {
-            0.45: 'rgb(0,255,0)',
-            0.65: 'rgb(64,192,0)',
-            0.75: 'rgb(192,64,0)',
-            1.0: 'rgb(255,0,0)'
+    var initialise_map = function (location) {
+        $('.status.success').show();
+
+        var latlon = [location.coords.latitude, location.coords.longitude];
+        var map = L.map('map', {
+            center: latlon,
+            zoom: 13
+        });
+        var marker = L.marker(latlon).addTo(map);
+
+        update_map(map);
+        send_location_to_application(map);
+    };
+
+
+    var update_map = function (map, data) {
+        data = data || null;
+
+        var google_maps_layer = new L.Google('TERRAIN');
+        if (!map.hasLayer(google_maps_layer)) {
+            map.addLayer(google_maps_layer);
         }
-    });
 
-    heatmapLayer.setData(data);
+        if (data) {
+            for (var i=0; i<data.length; i++) {
+                var datum = data[i];
+                var color = 'green';
+                if (datum.value > 0.4) {
+                    color = 'yellow';
+                }
+                if (datum.value > 0.8) {
+                    color = 'red';
+                }
 
-    for (var i = 0; i < data.length; i++) {
-      var datum = data[i];
-      var color = 'green';
-      if (datum.value > 0.4) {
-        color = 'yellow';
-      }
-      if (datum.value > 0.8) {
-        color = 'red';
-      }
-      var m = L.circle([datum.lat, datum.lon], 200, {
-        color: color,
-        fillColor: color,
-        fillOpacity: 0.5
-      }).addTo(map);
-    }
-    //map.addLayer(heatmapLayer);
-};
-var send_location = function (map) {
-    var bounds = map.getBounds();
-    var latlng = map.getCenter();
-    var url = '/data/'+latlng.lat+','+latlng.lng+'/'+bounds._southWest.lat+','+bounds._southWest.lng+'/'+bounds._northEast.lat+','+bounds._northEast.lng;
-    $.ajax({
-        url: url
-    }).done(function (data){
-        generate_heatmap(map, data.data);
-    });
-};
-get_location();
+                var sensor = L.circle(
+                    [datum.lat, datum.lon],
+                    200,
+                    {
+                        color: color,
+                        fillColor: color,
+                        fillOpacity: 0.5
+                    }).addTo(map);
+            }
+        }
+    };
+
+
+    module.init = function () {
+        if (Modernizr.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                initialise_map,
+                handle_location_error
+            );
+        }
+    };
+
+})(core);
+
+core.init();
