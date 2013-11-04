ac5656e3d73dabc7c8b236c1a5ce45821cd46cf2
Max Glenister <moglenstar@gmail.com>
2013-11-04 16:07:54 +0000

    Removed the test for FLOOD_DATA_API_URL/KEY from oxfloodnet.__init__ as we're not using the xively data source anymore.

--- oxfloodnet/__init__.py
+++ oxfloodnet/__init__.py
@@ -9,14 +9,7 @@ oxfloodnet.config['FREEZER_DESTINATION'] = os.path.dirname(os.path.abspath(__fil
 # Function to easily find your assets
 # In your template use <link rel=stylesheet href="{{ static('filename') }}">
 oxfloodnet.jinja_env.globals['static'] = (
-	lambda filename: url_for('static', filename = filename)
+    lambda filename: url_for('static', filename = filename)
 )
-
-# Data source config
-try:
-    oxfloodnet.config['FLOOD_DATA_API_URL'] = os.environ['FLOOD_DATA_API_URL']
-    oxfloodnet.config['FLOOD_DATA_API_KEY'] = os.environ['FLOOD_DATA_API_KEY']
-except KeyError, e:
-    raise Exception, "You must define the environment variable %s before running, with e.g. 'export %s=...'" % (e,e)
 
 from oxfloodnet import views
